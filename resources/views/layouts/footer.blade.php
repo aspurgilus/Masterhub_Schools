31692710486c8bdc42ff3987360c30321076ba29
Yaroslav <barsinvest84@gmail.com>
2019-02-05 20:17:38 +0200

    Creating new project

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,1 +1,4 @@
+<div class="card-footer footer">
+<div class="links"><a href="https://laravel.com/docs">Документация</a></div>
 <div>All rights reserved 2019</div>
+</div>
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,1 +1,13 @@
-<div>Masterhub header</div>
+@if (Route::has('login'))
+    <div class="top-right links">
+        @auth
+            <a href="{{ url('/home') }}">Home</a>
+        @else
+            <a href="{{ route('login') }}">Login</a>
+
+            @if (Route::has('register'))
+                <a href="{{ route('register') }}">Register</a>
+            @endif
+        @endauth
+    </div>
+@endif
